1369946ea2772fbbe37df113d8c5311e07ce8a25
nvpletnev <nvpletnev@gmail.com>
2018-07-04 22:12:31 +0300

    Create TestClass TestClassTest with method remove

---
+++ src/collections/abstractCollection/TestClass.java
@@ -0,0 +1,146 @@
+package collections.abstractCollection;
+
+import java.util.*;
+
+import java.util.Iterator;
+
+public class TestClass<T> implements Collection<T> {
+
+    private T[] m = (T[])new Object[1];
+
+    private int size;
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size() == 0;
+    }
+
+    @Override
+    public boolean contains(final Object o) {
+        for (int i = 0; i < size; i++) {
+            if (m[i].equals(o)) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ElementsIterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        final T[] newM = (T[])new Object[this.size()];
+        System.arraycopy(m, 0, newM, 0, this.size());
+        return newM;
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return (T1[])this.toArray();
+    }
+
+    @Override
+    public boolean add(final T t) {
+        if (m.length == size) {
+            final T[] oldM = m;
+            m = (T[]) new Object[this.size() * 2];
+            System.arraycopy(oldM, 0, m, 0, oldM.length);
+        }
+        m[size++] = t;
+        return true;
+    }
+
+    @Override
+    public boolean remove(final Object o) {
+        for (int i = 0; i < size(); i++) {
+            if (m[i].equals(o)) {
+                this.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(final Collection<?> c) {
+        for (final Object item : c) {
+            if (!this.contains(item)) return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(final Collection<? extends T> c) {
+        for (final T item : c) {
+            add(item);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(final Collection<?> c) {
+        for (final Object item : c) {
+            remove(item);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean retainAll(final Collection<?> c) {
+        for (final Object item : this) {
+            if (!c.contains(item)) this.remove(item);
+        }
+        return true;
+    }
+
+    @Override
+    public void clear() {
+        m = (T[])new Object[1];
+        size = 0;
+    }
+
+    private void remove(final int index) {
+        if (index != this.size() - 1)
+            System.arraycopy(m, index + 1, m, index, this.size() - index - 1);
+        if (this.size() != 0) { //fix
+            size--;
+        }
+    }
+
+    private class ElementsIterator implements Iterator<T> {
+
+        private int count;
+
+        @Override
+        public boolean hasNext() {
+            return count < TestClass.this.size();
+        }
+
+        @Override
+        public T next() {
+
+            if (count < TestClass.this.size()) {
+                return TestClass.this.m[count++];
+            }
+            else throw new NoSuchElementException();
+        }
+
+        //TODO
+        @Override
+        public void remove() {
+
+            if (count > TestClass.this.size() || next().equals(null)) {
+                throw new IllegalStateException();
+            }
+            else TestClass.this.remove(TestClass.this.size() - 1);
+        }
+
+
+    }
+}
